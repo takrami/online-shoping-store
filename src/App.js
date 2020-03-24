e73d8918a25f95f25da917996b40a9483c72bbb4
Atefeh H.Takrami <ati.takrami@gmail.com>
2020-03-24 16:28:13 +0100

    move app's shop data into our firestore database,  create a WithSpinner HOC for asynchronous loading

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ class App extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser
+  currentUser: selectCurrentUser,
 });
 
 const mapDispatchToProps = dispatch => ({
